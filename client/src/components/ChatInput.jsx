33840b8a40625d7386af6b7f096dfac70b7a676d
Danish <dan71ish@gmail.com>
2024-03-23 19:46:49 +0530

    input component

--- client/src/components/ChatInput.jsx
+++ client/src/components/ChatInput.jsx
@@ -1,7 +1,18 @@
 const ChatInput = () => {
   return (
-    <div className="bg-red-300 fixed bottom-3 left-1/2 translate-x-[-50%]">
-      <input  type="text" />
+    <div className="fixed bottom-2 w-full flex items-center justify-center">
+      <div className="w-full max-w-[750px] px-4 py-2">
+        <div className="relative inline-block w-full">
+          <input
+            type="text"
+            className="w-full pr-[40px] py-4 pl-4 rounded-lg focus:outline-none"
+            placeholder="Enter a prompt here"
+          />
+          <button className="absolute top-[25%] right-4 border-none cursor-pointer pointer-events-none">
+            <i className="fi fi-rr-paper-plane-top"></i>{" "}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
