cdc5485ec11a9b345843a5bcf144893d1b2762f4
Danish <dan71ish@gmail.com>
2024-03-24 00:34:03 +0530

    made it working now will do more tweaks

--- client/src/components/ChatBody.jsx
+++ client/src/components/ChatBody.jsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { ChatContext } from "../App";
+
 const data = [
   { message: "What is the weather in banglore", iconClass: "fi fi-ts-clouds" },
   {
@@ -15,35 +18,47 @@ const data = [
 ];
 
 const ChatBody = () => {
+  const { chat, setChat } = useContext(ChatContext);
   return (
     <>
       <div className="max-w-[1050px] mx-auto mt-16 md:mt-8 px-4">
-        <div>
-          <h1 className="text-5xl ">Hello, Danish</h1>
-          <h2 className="text-4xl mt-1 md:text-5xl text-gray-600 font-medium">
-            How can I help you today?
-          </h2>
+        {!chat.length ? (
+          <div>
+            <h1 className="text-5xl ">Hello, Danish</h1>
+            <h2 className="text-4xl mt-1 md:text-5xl text-gray-600 font-medium">
+              How can I help you today?
+            </h2>
 
-          <div className="flex overflow-auto  mt-8 gap-4">
-            {data.map((item, i) => {
-              return (
-                <div
-                  className="bg-zinc-700 py-2 px-4 rounded-lg h-auto md:text-xl md:h-[150px]"
-                  key={i}
-                >
-                  <p className="text-md">{item.message}</p>
-                  <i className={item.iconClass}></i>
-                </div>
-              );
-            })}
+            <div className="flex overflow-auto  mt-8 gap-4">
+              {data.map((item, i) => {
+                return (
+                  <div
+                    className="bg-zinc-700 py-2 px-4 rounded-lg h-auto md:text-xl md:h-[150px]"
+                    key={i}
+                  >
+                    <p className="text-md">{item.message}</p>
+                    <i className={item.iconClass}></i>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>
+        ) : (
+          ""
+        )}
 
         <div className="mt-1">
-          <div className="flex gap-2">
-            <p className="w-6 h-6 rounded-full bg-orange-500"></p>
-            <p>My Prompt</p>
-          </div>
+          {chat.map((item, i) => {
+            return (
+              <div key={i} className="mt-4">
+                <div className="flex gap-2 items-center">
+                  <p className="w-6 h-6 rounded-full bg-orange-500"></p>
+                  <p>{item.author}</p>
+                </div>
+                <p className="pl-12 mt-2">{item.message}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
